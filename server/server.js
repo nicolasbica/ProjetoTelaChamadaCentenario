c84a57e603ae4f1c17a78f1f06b4600fa4aebded
nicolas-bica <swordk664@gmail.com>
2025-11-27 21:42:29 -0300

    agora vai2

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 // Middleware
 app.use(express.json());
